0dcc917460a9dee788c824924d3cb18d5ed455bd
Andrew Lamont <andrew.m.lamont@gmail.com>
2020-05-14 12:51:36 -0400

    implemented directionality collapse, transitive reduction

--- con.py
+++ con.py
@@ -30,14 +30,15 @@ class Trochee:
 		self.name = 'Trochee' + direction
 
 	def vios(self, candidate):
-		loci = []
-
+		loci = [0 for segment in candidate if segment in alph.alphabet]
+		seg = 0
 		for i in range(len(candidate)):
 			if candidate[i] in alph.alphabet:
-				if candidate[i-1:i+4] == "(L'L)" or candidate[i-3:i+2] == "(L'L)":
-					loci.append(1)
-				else:
-					loci.append(0)
+				if i > 1:
+					if candidate[i-1] == "'":
+						if candidate[i-2] != '(':
+							loci[seg] = 1
+				seg += 1
 
 		if self.direction == 'N': return [sum(loci)]
 		if self.direction == 'R': return loci[::-1]
@@ -50,34 +51,256 @@ class Iamb:
 		self.name = 'Iamb' + direction
 
 	def vios(self, candidate):
-		loci = []
-
+		loci = [0 for segment in candidate if segment in alph.alphabet]
+		seg = 0
 		for i in range(len(candidate)):
 			if candidate[i] in alph.alphabet:
-				if candidate[i-1:i+4] == "('LL)" or candidate[i-3:i+2] == "('LL)":
-					loci.append(1)
-				else:
-					loci.append(0)
+				if 0 < i < len(candidate) - 1:
+					if candidate[i-1] == "'":
+						if candidate[i+1] != ')':
+							loci[seg] = 1
+				seg += 1
 
 		if self.direction == 'N': return [sum(loci)]
 		if self.direction == 'R': return loci[::-1]
 		return loci
 
-# FtBin
+# FootForm -- combines Trochee and Iamb into one constraint
+class FootForm:
+	def __init__(self, type, direction):
+		self.type = type
+		self.direction = direction
+		self.name = 'FootForm' + type + direction
+
+	def vios(self, candidate):
+		if self.type == 'Trochee':
+			loci = [0 for segment in candidate if segment in alph.alphabet]
+			seg = 0
+			for i in range(len(candidate)):
+				if candidate[i] in alph.alphabet:
+					if i > 1:
+						if candidate[i-1] == "'":
+							if candidate[i-2] != '(':
+								loci[seg] = 1
+					seg += 1
+		elif self.type == 'Iamb':
+			loci = [0 for segment in candidate if segment in alph.alphabet]
+			seg = 0
+			for i in range(len(candidate)):
+				if candidate[i] in alph.alphabet:
+					if 0 < i < len(candidate) - 1:
+						if candidate[i-1] == "'":
+							if candidate[i+1] != ')':
+								loci[seg] = 1
+					seg += 1
+
+		if self.direction == 'N': return [sum(loci)]
+		if self.direction == 'R': return loci[::-1]
+		return loci
+
+# FtBin v1 -- satisfied by (H)
 class FtBin:
 	def __init__(self, direction):
 		self.direction = direction
 		self.name = 'FtBin' + direction
 
 	def vios(self, candidate):
-		loci = []
+		loci = [0 for segment in candidate if segment in alph.alphabet]
+		seg = 0
 		for i in range(len(candidate)):
 			if candidate[i] in alph.alphabet:
-				if candidate[i-2:i+2] == "('L)":
-					loci.append(1)
-				else:
-					loci.append(0)
+				if 1 < i < len(candidate) - 1:
+					if candidate[i-1] == "'":
+						if candidate[i-2] == '(':
+							if candidate[i+1] == ')':
+								if candidate[i] != 'H':
+									loci[seg] = 1
+				seg += 1
 
 		if self.direction == 'N': return [sum(loci)]
 		if self.direction == 'R': return loci[::-1]
 		return loci
+
+# FtBin v2 -- violated by (H)
+class FtBinSyll:
+	def __init__(self, direction):
+		self.direction = direction
+		self.name = 'FtBinSyll' + direction
+
+	def vios(self, candidate):
+		loci = [0 for segment in candidate if segment in alph.alphabet]
+		seg = 0
+		for i in range(len(candidate)):
+			if candidate[i] in alphabet:
+				if 1 < i < len(candidate) - 1:
+					if candidate[i-1] == "'":
+						if candidate[i-2] == '(':
+							if candidate[i+1] == ')':
+								loci[seg] = 1
+				seg += 1
+
+		if self.direction == 'N': return [sum(loci)]
+		if self.direction == 'R': return loci[::-1]
+		return loci
+
+# Clash -- Pruitt doesn't use this constraint
+class Clash:
+	def __init__(self, direction):
+		self.direction = direction
+		self.name = 'Clash' + direction
+
+	def vios(self, candidate):
+		cand = candidate.replace('(', '').replace(')', '')
+		loci = [0 for segment in cand if segment in alph.alphabet]
+		seg = 0
+		for i in range(len(cand)):
+			if cand[i] in alphabet:
+				if 0 < i < len(cand) - 1:
+					if cand[i-1] == "'":
+						if cand[i+1] == "'":
+							loci[seg] = 1
+				seg += 1
+
+		if self.direction == 'N': return [sum(loci)]
+		if self.direction == 'R': return loci[::-1]
+		return loci
+
+# Edgefoot -- assign a violation if the left/right edge is unfooted
+class EdgeFoot:
+	def __init__(self, direction):
+		self.direction = direction
+		self.name = 'EdgeFoot' + direction
+
+	def vios(self, candidate):
+		loci = [0]
+		if self.direction == 'L':
+			for segment in candidate:
+				if segment in alph.alphabet:
+					if segment == segment.lower():
+						loci[0] = 1
+					break
+		else:
+			for segment in candidate[::-1]:
+				if segment in alph.alphabet:
+					if segment == segment.lower():
+						loci[0] = 1
+					break
+
+		return loci
+
+# NonFinality -- assign a violation if the rightmost syllable is stressed
+class NonFinality:
+	def __init__(self, direction):
+		self.direction = direction
+		self.name = 'NonFinality' + direction
+
+	def vios(self, candidate):
+		loci = [0]
+		if len(candidate) > 2:
+			if candidate[-3] == "'":
+				loci[0] = candidate[1]
+
+		if self.direction == 'N': return [sum(loci)]
+		return loci
+
+# AllFt-L: For each foot in a word assign one violation for every syllable separating it from the left edge of the word
+class AllFtL:
+	def __init__(self):
+		self.direction = 'N'
+		self.name = 'AllFtL'
+
+	def vios(self, candidate):
+		loci = 0
+		for i in range(len(candidate)):
+			if candidate[i] == '(':
+				for j in range(i):
+					if candidate[j] in alph.alphabet:
+						loci += 1
+		return loci
+
+# AllFt-R: For each foot in a word assign one violation for every syllable separating it from the right edge of the word
+class AllFtR:
+	def __init__(self):
+		self.direction = 'N'
+		self.name = 'AllFtR'
+
+	def vios(self, candidate):
+		loci = 0
+		for i in range(len(candidate)):
+			if candidate[i] == ')':
+				for j in range(i+1, len(candidate)):
+					if candidate[j] in alph.alphabet:
+						loci += 1
+		return loci
+
+# Nate Koser align L: *syllable(Foot)
+class SyllFt:
+	def __init__(self):
+		self.direction = 'N'
+		self.name = 'SyllFt'
+
+	def vios(self, candidate):
+		loci = 0
+		for i in range(1,len(candidate)):
+			if candidate[i] == '(':
+				if candidate[i-1] in alph.alphabet:
+					loci += 1
+		return loci
+
+# Nate Koser align R: *(Foot)syllable
+class FtSyll:
+	def __init__(self):
+		self.direction = 'N'
+		self.name = 'FtSyll'
+
+	def vios(self, candidate):
+		loci = 0
+		for i in range(len(candidate) - 1):
+			if candidate[i] == ')':
+				if candidate[i+1] in alph.alphabet:
+					loci += 1
+		return loci
+
+# *Foot -- don't have feet
+class NoFeet:
+	def __init__(self, direction):
+		self.direction = direction
+		self.name = 'NoFeet' + direction
+
+	def vios(self, candidate):
+		if self.direction == 'N':
+			loci = 0
+			for i in range(len(candidate)):
+				if candidate[i] == '(':
+					loci += 1
+			return loci
+		else:
+			loci = [' ' for segment in candidate if segment in alph.alphabet]
+			seg = 0
+			for i in range(len(candidate)):
+				if candidate[i] == '(':
+					loci[seg] = '('
+					seg += 1
+			if self.direction == 'R':
+				return loci[::-1]
+			return loci
+
+# HaveStress -- have at least one stress
+class HaveStress:
+	def __init__(self, direction):
+		self.direction = direction
+		self.name = 'HaveStress' + direction
+
+	def vios(self, candidate):
+		if self.direction == 'N':
+			loci = 0
+			if '(' not in candidate:
+				loci = 1
+			return loci
+		else:
+			loci = [' ']
+			if '(' not in candidate:
+				loci[0] = '('
+
+			# Direction doesn't matter -- there is exactly one locus of violation
+			return loci
--- typologizer.py
+++ typologizer.py
@@ -1,10 +1,10 @@
 import sys
-from sympy import *
 import alph
 from con import *
 from gen import gen
 from tablO import tablO
 from fred import FRed
+from itertools import combinations
 
 # command line parameter to control printing
 verbose = '-v' in sys.argv
@@ -28,60 +28,175 @@ def ERCs(cv):
 		WL = []
 		for w in W:
 			WL.append(str(w) + '>>' + str(l))
-		WLs.append(WL)
+		WLs.append(tuple(WL))
 
 	return WLs
 
 alpha = ['l', 'L']
 alph.initialize(alpha)
-urs = ['llllll', 'lllllll']
+urs = ['ll','llllll', 'lllllll']
 
 con = [ParseSyllable('L'), ParseSyllable('R'), Trochee('L'), Trochee('R'), Iamb('L'), Iamb('R'), FtBin('L'), FtBin('R')]
 
-for input in urs:
-	print(input)
+# get all derivations for each ur
+derivations = []
 
-	# Generate candidates
-	candidates = gen(input)
+# ([FNF], derivation)
+for ur in urs:
+	stack = []
+	stack.append(([], (ur,)))
 
-	# Assemble tableau
-	tableau = []
-	for constraint in con:
-		tableau.append([constraint.vios(candidate) for candidate in candidates])
+	while stack:
+		derivation = stack.pop()
 
-	# Find minimal violation for all constraints
-	viominima = [tableau[c][0][:] for c in range(len(con))]
+		# check for convergence
+		if len(derivation[1]) > 1 and derivation[1][-2] == derivation[1][-1]:
+			derivations.append(derivation)
+			continue
 
-	for c in range(1, len(candidates)):
-		for v in range(len(con)):
-			if not leq(viominima[v], tableau[v][c]):
-					viominima[v] = tableau[v][c][:]
+		# Generate candidate set
+		input = derivation[1][-1]
+		candidates = gen(input)
 
-	# Iterate through candidates
-	for optimum in range(len(candidates)):
+		# Assemble tableau
+		tableau = []
+		for constraint in con:
+			tableau.append([constraint.vios(candidate) for candidate in candidates])
 
-		# Generate comparative tableau
-		comptableau = []
-		for c in range(len(candidates)):
-			row = [''] * len(con)
+		# Find minimal violation for all constraints
+		viominima = [tableau[c][0][:] for c in range(len(con))]
+
+		for c in range(1, len(candidates)):
 			for v in range(len(con)):
-				if leq(tableau[v][optimum], tableau[v][c]) and leq(tableau[v][c], tableau[v][optimum]):
-					row[v] = 'e'
-				elif leq(tableau[v][c], tableau[v][optimum]):
-					row[v] = 'L'
-				else:
-					row[v] = 'W'
-			comptableau.append(row)
+				if not leq(viominima[v], tableau[v][c]):
+						viominima[v] = tableau[v][c][:]
 
-		#tablO(tableau, input, candidates, con, optimum, comptableau)
+		# Iterate through candidates
+		for optimum in range(len(candidates)):
 
-		fnf = FRed(comptableau[:optimum] + comptableau[optimum + 1:])
-		print(candidates[optimum])
-		if 'unsat' not in fnf:
-			ercs = []
-			for x in fnf:
-				ercs += ERCs(x)
-			print(ercs)
-		else:
-			print('unsatisfiable!')
-		print()
+			# Generate comparative tableau
+			comptableau = []
+			for c in range(len(candidates)):
+				row = [''] * len(con)
+				for v in range(len(con)):
+					if leq(tableau[v][optimum], tableau[v][c]) and leq(tableau[v][c], tableau[v][optimum]):
+						row[v] = 'e'
+					elif leq(tableau[v][c], tableau[v][optimum]):
+						row[v] = 'L'
+					else:
+						row[v] = 'W'
+				comptableau.append(row)
+
+			#tablO(tableau, input, candidates, con, optimum, comptableau)
+
+			# Run FRed on optimum
+			FNF = FRed(comptableau[:optimum] + comptableau[optimum + 1:])
+
+			if 'unsat' not in FNF:
+				# combine ranking arguments
+				combinedFNF = derivation[0][:]
+				for fnf in FNF:
+					combinedFNF.append(list(fnf))
+
+				# check for inconsistency
+				newFNF = FRed(combinedFNF)
+				if 'unsat' not in newFNF:
+					newderivation = (list(newFNF)[:], derivation[1] + (candidates[optimum],))
+					stack.append(newderivation)
+
+typology = []
+
+# combine derivations
+combined = combinations(derivations, len(urs))
+for combo in combined:
+	comboFNF = []
+	for derivation in combo:
+		comboFNF += derivation[0][:]
+
+	derivs = [derivation[1] for derivation in combo]
+
+	# check for inconsistency
+	newFNF = FRed(comboFNF)
+	if 'unsat' not in newFNF:
+		language = (list(newFNF)[:], derivs)
+		typology.append(language)
+
+for language in typology:
+	for chain in language[1]:
+		print(chain[0],'\t',chain[-1])
+
+	ERCS = set()
+	for cv in language[0]:
+		ERC = ERCs(cv)
+		for erc in ERC:
+			ERCS.add(erc)
+
+	# collapse ercs on direction
+	collapseset = set()
+	for constraint in con:
+		if constraint.direction in ['L', 'R']:
+			n = constraint.name[:-1]
+			d = constraint.direction
+			b = 'L'
+			if d == 'L': b = 'R'
+			dset = set()
+			bset = set()
+			for erc in ERCS:
+				for e in erc:
+					espl = [con[int(x)].name for x in e.split('>>')]
+					if espl[0][:-1] == espl[1][:-1]: continue
+					if espl[0][:-1] == n:
+						if espl[0][-1] == d:
+							dset.add(('',espl[1]))
+						elif espl[0][-1] == b:
+							bset.add(('',espl[1]))
+					if espl[1][:-1] == n:
+						if espl[1][-1] == d:
+							dset.add((espl[0],''))
+						elif espl[1][-1] == b:
+							bset.add((espl[0],''))
+			if dset == bset:
+				collapseset.add(n)
+	collapsedERCS = {}
+	for erc in ERCS:
+		for e in erc:
+			espl = [con[int(x)].name for x in e.split('>>')]
+			if espl[0][:-1] in collapseset:
+				espl[0] = espl[0][:-1]
+			if espl[1][:-1] in collapseset:
+				espl[1] = espl[1][:-1]
+			if espl[0] not in collapsedERCS:
+				collapsedERCS[espl[0]] = set()
+			collapsedERCS[espl[0]].add(espl[1])
+
+	# remove transitive edges
+	visited = set()
+
+	def visit(v):
+		if v not in visited:
+			visited.add(v)
+			indirect = set()
+			if v in collapsedERCS:
+				for w in collapsedERCS[v]:
+					visit(w)
+					# run bfs to find descendents of w
+					queue = [w]
+					discovered = set()
+					while queue:
+						x = queue.pop()
+						if x in collapsedERCS:
+							for y in collapsedERCS[x]:
+								if y not in discovered:
+									discovered.add(y)
+									queue = [y] + queue
+					indirect = indirect.union(discovered)
+				for ind in indirect:
+					collapsedERCS[v].remove(ind)
+
+	for v in collapsedERCS:
+		visit(v)
+
+	for ce in collapsedERCS:
+		print(ce, '\t', '\t'.join(collapsedERCS[ce]))
+
+	print()
